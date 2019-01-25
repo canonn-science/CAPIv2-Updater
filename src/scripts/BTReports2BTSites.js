45415ce9a14608e0beaaad33dd68f998c9478508
DMehaffy <derrickmehaffy@gmail.com>
2019-01-25 09:42:15 -0700

    import is important!

--- src/scripts/BTReports2BTSites.js
+++ src/scripts/BTReports2BTSites.js
@@ -6,6 +6,8 @@ import { REPORT_STATUS } from '../settings';
 
 import { LOCALE, TIMEZONE } from '../settings';
 
+import site_from_report from '../../updaters/site_from_report';
+
 // Import UI console printers for consistent script look
 import { 
 	UI_header, 
