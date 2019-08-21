6780cafb0ed26302a7d8824d01c559ffcf14bdbe
Derrick Mehaffy <derrickmehaffy@gmail.com>
2019-08-21 00:10:22 -0700

    setting predefined URLs to call via an environment variable

--- src/settings.js
+++ src/settings.js
@@ -32,11 +32,28 @@ export const ERR429_DELAY =  300000; // 5 min
 
 /* Canonn APIv2 Options */
 
-// Canonn API urls - This is currently configured to work on the development server
-export const API_CANONN_GRAPHQL = 'https://api.canonn.tech:2083/graphql';
-export const API_CANONN_REST = 'https://api.canonn.tech:2083';
-//export const API_CANONN_GRAPHQL = 'http://localhost:1339/graphql';
-//export const API_CANONN_REST = 'http://localhost:1339';
+
+// Canonn API urls
+let CANONN_GRAPHQL = null
+let CANONN_REST = null
+
+// Preset URLs for Production, Staging, Development, and Local servers
+if (process.env.CAPI_SERVER == 'production') {
+	CANONN_GRAPHQL = 'https://api.canonn.tech/graphql';
+	CANONN_REST = 'https://api.canonn.tech';
+} else if (process.env.CAPI_SERVER == 'staging') {
+	CANONN_GRAPHQL = 'https://api.canonn.tech:2053/graphql';
+	CANONN_REST = 'https://api.canonn.tech:2053';
+} else if (process.env.CAPI_SERVER == 'development') {
+	CANONN_GRAPHQL = 'https://api.canonn.tech:2083/graphql';
+	CANONN_REST = 'https://api.canonn.tech:2083';
+} else {
+	CANONN_GRAPHQL = 'http://localhost:1339/graphql';
+	CANONN_REST = 'http://localhost:1339';
+}
+
+export const API_CANONN_GRAPHQL = CANONN_GRAPHQL
+export const API_CANONN_REST = CANONN_REST
 
 // max limit per one GraphQL query for Canonn API
 // consult with your nearest Canonn API admin
